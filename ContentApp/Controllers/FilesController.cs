29ad0dd11b384f972583d640b888ded428160157
Mike Dice <mikedice417@hotmail.com>
2020-06-07 17:40:02 -0700

    populate files

---
+++ ContentApp/Controllers/FilesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContentApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        // GET: api/Files
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Files/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Files
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Files/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpPost("files")]
+        public async Task<IActionResult> Images(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        using (var memStream = new MemoryStream())
+                        {
+                            await formFile.CopyToAsync(memStream);
+                            memStream.Seek(0, SeekOrigin.Begin);
+                            var fileName = FileStorage.StoreImage(memStream);
+                        }
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size });
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ContentApp/FileStorage.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Auth;
+using Microsoft.Azure.Storage.Blob;
+
+namespace ContentApp
+{
+    public class FileStorage
+    {
+        private static string connection = "DefaultEndpointsProtocol=https;AccountName=contentstorage417;AccountKey=iLqvukxK9cz3l3Jp2g6QWO2teiTfXbwLQ2WoBY6xt+CDw67/K/CEOgGkjS2LusqjRIwnZDWVF6jqDkOEtYc9qw==;EndpointSuffix=core.windows.net";
+        private const string ImageKeyTiny = "tiny";
+        private const string ImageKeyMedium = "medium";
+        private const string ImageKeyOriginal = "normal";
+        private const int OrientationKey = 0x0112;
+        private const int NotSpecified = 0;
+        private const int NormalOrientation = 1;
+        private const int MirrorHorizontal = 2;
+        private const int UpsideDown = 3;
+        private const int MirrorVertical = 4;
+        private const int MirrorHorizontalAndRotateRight = 5;
+        private const int RotateLeft = 6;
+        private const int MirorHorizontalAndRotateLeft = 7;
+        private const int RotateRight = 8;
+
+        private static Image ScaleImage(Image original, double scale)
+        {
+            var newWidth = Math.Floor(original.Width * scale);
+            var newHeight = Math.Floor(original.Height * scale);
+            Bitmap newImage = new Bitmap((int)newWidth, (int)newHeight, PixelFormat.Format32bppArgb);
+            using (var graphics = Graphics.FromImage(newImage)) 
+            {
+                //graphics.Clear(Color.Transparent);
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+                graphics.DrawImage(original,
+                    new Rectangle(0, 0, (int)newWidth, (int)newHeight),
+                    new Rectangle(0, 0, original.Width, original.Height),
+                    GraphicsUnit.Pixel);
+
+                if (original.PropertyIdList.Contains(OrientationKey))
+                {
+                    var orientation = (int)original.GetPropertyItem(OrientationKey).Value[0];
+                    switch (orientation)
+                    {
+                        case NotSpecified: // Assume it is good.
+                        case NormalOrientation:
+                            // No rotation required.
+                            break;
+                        case MirrorHorizontal:
+                            newImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                            break;
+                        case UpsideDown:
+                            newImage.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                            break;
+                        case MirrorVertical:
+                            newImage.RotateFlip(RotateFlipType.Rotate180FlipX);
+                            break;
+                        case MirrorHorizontalAndRotateRight:
+                            newImage.RotateFlip(RotateFlipType.Rotate90FlipX);
+                            break;
+                        case RotateLeft:
+                            newImage.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                            break;
+                        case MirorHorizontalAndRotateLeft:
+                            newImage.RotateFlip(RotateFlipType.Rotate270FlipX);
+                            break;
+                        case RotateRight:
+                            newImage.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                            break;
+                        //default:
+                            //throw new NotImplementedException("An orientation of " + orientation + " isn't implemented.");
+                    }
+                }
+            }
+            return newImage;        
+        }
+
+        private static Dictionary<string, Image> PrepareImages(Stream data) 
+        {
+            Dictionary<string, Image> result = new Dictionary<string, Image>();
+            
+            var original = Image.FromStream(data, false, false);
+            result.Add(ImageKeyTiny, ScaleImage(original, 0.1));
+            result.Add(ImageKeyMedium, ScaleImage(original, 0.3));
+            result.Add(ImageKeyOriginal, original);
+            return result;
+        }
+
+        
+        public static string StoreImage(Stream data)
+        {
+            var imageDictionary = PrepareImages(data);
+            var fileBase = Guid.NewGuid().ToString();
+            if (CloudStorageAccount.TryParse(connection, out var storageAccount))
+            {
+                CloudBlobClient client = storageAccount.CreateCloudBlobClient();
+
+                var container = client.GetContainerReference("images");
+
+                foreach (var kvp in imageDictionary)
+                {
+                    var fileName = $"{fileBase}.{kvp.Key}.jpg";
+                    var blockBlob = container.GetBlockBlobReference(fileName);
+                    blockBlob.Properties.ContentType = "image/jpeg";
+                    using (var memStream = new MemoryStream())
+                    {
+                        kvp.Value.Save(memStream, ImageFormat.Jpeg);
+                        memStream.Flush();
+                        memStream.Seek(0, SeekOrigin.Begin);
+                        blockBlob.UploadFromStream(memStream);
+                    }
+                }
+                return fileBase;
+            }
+            return null;
+        }
+        
+        /*
+        public static string StoreImage(Stream data)
+        {
+            var fileBase = Guid.NewGuid().ToString();
+            if (CloudStorageAccount.TryParse(connection, out var storageAccount))
+            {
+                CloudBlobClient client = storageAccount.CreateCloudBlobClient();
+
+                var container = client.GetContainerReference("images");
+
+                    var fileName = $"{fileBase}.{ImageKeyOriginal}.jpg";
+                    var blockBlob = container.GetBlockBlobReference(fileName);
+                    blockBlob.Properties.ContentType = "image/jpeg";
+                    blockBlob.UploadFromStream(data);
+                
+                return fileBase;
+            }
+            return null;
+        }
+        */
+
+    }
+}
